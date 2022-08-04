415cf86bb4ffaa8404030073e6b728315ef97d61
Nate S <seilaffn@gmail.com>
2022-08-04 09:08:39 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hi whats your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}, Good to see you here. ");
